7c35013904ddb1673e4d529f1f374c0f447989bb
flying sheep <flying-sheep@web.de>
2011-12-19 20:03:08 +0100

    upgraded to 6-tiling

--- src/tiling/Cardinal.java
+++ src/tiling/Cardinal.java
@@ -1,74 +1,33 @@
 package tiling;
 
-public enum Cardinal {
-	   TOPLEFT(Horizontal.LEFT, Vertical.TOP),       TOP(Horizontal.CENTER, Vertical.TOP),       TOPRIGHT(Horizontal.RIGHT, Vertical.TOP),
-	      LEFT(Horizontal.LEFT, Vertical.MIDDLE), CENTER(Horizontal.CENTER, Vertical.MIDDLE),       RIGHT(Horizontal.RIGHT, Vertical.MIDDLE),
-	BOTTOMLEFT(Horizontal.LEFT, Vertical.BOTTOM), BOTTOM(Horizontal.CENTER, Vertical.BOTTOM), BOTTOMRIGHT(Horizontal.RIGHT, Vertical.BOTTOM);
-	
-	public final Horizontal h;
-	public final Vertical   v;
-	
-	Cardinal(Horizontal h, Vertical v) {
-		this.h = h;
-		this.v = v;
-	}
-	
-	public static Cardinal valueOf(Horizontal h, Vertical v) throws ValueNotCoveredException {
-		switch (h) {
-		case LEFT:
-			switch (v) {
-			case TOP:
-				return TOPLEFT;
-			case MIDDLE:
-				return LEFT;
-			case BOTTOM:
-				return BOTTOMLEFT;
-			}
-		case CENTER:
-			switch (v) {
-			case TOP:
-				return TOP;
-			case MIDDLE:
-				return CENTER;
-			case BOTTOM:
-				return BOTTOM;
-			}
-		case RIGHT:
-			switch (v) {
-			case TOP:
-				return TOPRIGHT;
-			case MIDDLE:
-				return RIGHT;
-			case BOTTOM:
-				return BOTTOMRIGHT;
-			}
-		}
-		throw new ValueNotCoveredException(h, v);
-	}
-
-	public enum Horizontal {
+public class Cardinal {
+	public static enum Horizontal {
 		LEFT(-1), CENTER(0), RIGHT(1);
 		public final int i;
-		Horizontal(int i) {this.i = i;}
+		
+		Horizontal(int i) { this.i = i; }
+		
 		public static Horizontal valueOf(int i) throws ValueNotCoveredException {
 			switch (i) {
-			case -1: return LEFT;
-			case  0: return CENTER;
-			case  1: return RIGHT;
+				case -1: return LEFT;
+				case  0: return CENTER;
+				case  1: return RIGHT;
 			}
 			throw new ValueNotCoveredException(i);
 		}
 	}
-
-	public enum Vertical {
+	
+	public static enum Vertical {
 		TOP(-1), MIDDLE(0), BOTTOM(1);
 		public final int i;
-		Vertical(int i) {this.i = i;}
+		
+		Vertical(int i) { this.i = i; }
+		
 		public static Vertical valueOf(int i) throws ValueNotCoveredException {
 			switch (i) {
-			case -1: return TOP;
-			case  0: return MIDDLE;
-			case  1: return BOTTOM;
+				case -1: return TOP;
+				case  0: return MIDDLE;
+				case  1: return BOTTOM;
 			}
 			throw new ValueNotCoveredException(i);
 		}
@@ -85,16 +44,13 @@ public enum Cardinal {
 			this.v = v;
 		}
 		
-		public ValueNotCoveredException(int i) {
-			this.i = i;
-		}
+		public ValueNotCoveredException(int i) { this.i = i; }
 		
 		@Override
 		public String getMessage() {
-			if (this.i != 0)
-				return "Direction " + i + " not covered";
-			else
-				return "Direction " + h.name().toLowerCase() + ", " + v.name().toLowerCase() + "not covered";
+			return "Direction " +
+				((this.i != 0) ? i : h.name().toLowerCase() + ", " + v.name().toLowerCase()) +
+				" not covered";
 		}
 	}
 }
--- src/tiling/Tiling.java
+++ src/tiling/Tiling.java
@@ -16,27 +16,28 @@ import tiling.QuarterTiledImage.QuarteredTile;
 public class Tiling {
 	static final int TILESIDE = 0x20;
 	
-	public static void main(String[] args) throws ValueNotCoveredException {		
-		final boolean[][] walldata = {
-			{false, true,  false},
-			{true,  true,  true},
-			{false, false, false},
-			{true,  true,  true},
-			{true,  true,  true},
-			{true,  true,  true}
-		};
+	public static void main(String[] args) throws ValueNotCoveredException {
+		final boolean[][] walldata = strs2barr(
+			"   X    ",
+			"  XXX   ",
+			" XXXXXX ",
+			" XX XXXX",
+			"XXX  XXX",
+			" XX  XXX",
+			"  XXXXX ",
+			"   XX   ");
 		
-		final int X = walldata[0].length -1;
-		final int Y = walldata   .length -1;
+		final int X = walldata[0].length - 1;
+		final int Y = walldata   .length - 1;
 		
 		final BufferedImage canvas = new BufferedImage((X+1)*TILESIDE, (Y+1)*TILESIDE, BufferedImage.TYPE_INT_ARGB);
 		final QuarterTiledImage wall = new QuarterTiledImage(canvas, TILESIDE, TILESIDE);
 		
 		for (int y=0; y<=Y; y++)
 			for (int x=0; x<=X; x++) {
+				QuarteredTile tile = wall.new QuarteredTile(x, y);
+				
 				if (walldata[y][x]) {
-					QuarteredTile tile = wall.new QuarteredTile(x, y); 
-					
 					final Surrounding occupied = new Surrounding();
 					
 					for (int dy=-1; dy<=1; dy++) {
@@ -45,7 +46,7 @@ public class Tiling {
 							
 							try {
 								o = walldata[y+dy][x+dx];
-							
+								
 								if (dx==  LEFT.i) o &= x>0;
 								if (dx== RIGHT.i) o &= x<X;
 								if (dy==   TOP.i) o &= y>0;
@@ -57,50 +58,56 @@ public class Tiling {
 							occupied.set(Horizontal.valueOf(dx), Vertical.valueOf(dy), o);
 							
 							if (dx != 0 || dy != 0)
-								System.out.print(o?"█":"░");
+								System.out.print(o?'█':'░');
 							else
-								System.out.print("▒");
+								System.out.print('▒');
 						}
 						System.out.println();
 					}
 					
-					for (Horizontal h : new Horizontal[] {LEFT, RIGHT})
-						for (Vertical v: new Vertical[] {BOTTOM, TOP})
+					for (Horizontal h : new Horizontal[] { LEFT, RIGHT })
+						for (Vertical v: new Vertical[] { BOTTOM, TOP })
 							if (occupied.get(CENTER, v))
 								if (occupied.get(h, MIDDLE))
 									if (occupied.get(h, v))
-										tile.setQuarter(h, v, FivePositions.EMPTY.getTile());
+										tile.setQuarter(h, v, SixPositions.NONE.getTile());
 									else
-										tile.setQuarter(h, v, FivePositions.CORNERS.getTile());
+										tile.setQuarter(h, v, SixPositions.CORNERS.getTile());
 								else
-									tile.setQuarter(h, v, FivePositions.SIDES.getTile());
+									tile.setQuarter(h, v, SixPositions.SIDES.getTile());
 							else
 								if (occupied.get(h, MIDDLE))
-									tile.setQuarter(h, v, FivePositions.FACES.getTile());
+									tile.setQuarter(h, v, SixPositions.FACES.getTile());
 								else
-									tile.setQuarter(h, v, FivePositions.ALL.getTile());
-					
-					System.out.println("———");
-				}
+									tile.setQuarter(h, v, SixPositions.ALL.getTile());
+				} else
+					for (Horizontal h : new Horizontal[] { LEFT, RIGHT })
+						for (Vertical v: new Vertical[] { BOTTOM, TOP })
+							tile.setQuarter(h, v, SixPositions.EMPTY.getTile());
+				
+				System.out.println("———");
 			}
 		
-        JOptionPane.showMessageDialog(null,
-        	new JLabel(
-        		new ImageIcon(wall.img),
-        		JLabel.CENTER
-        	), "Tiles!", -1);
+		JOptionPane.showMessageDialog(null, new JLabel(new ImageIcon(wall.img), JLabel.CENTER), "Tiles!", -1);
 	}
 	
-	private static enum FivePositions {
-		  ALL(0,0),   SIDES(1,0), EMPTY(2, 0),
-		FACES(0,1), CORNERS(1,1);
-		
+	private static boolean[][] strs2barr(String... strings) {
+		boolean[][] barr = new boolean[strings.length][strings[0].length()];
+		for (int s=0; s<strings.length; s++)
+			for (int c=0; c<strings[0].length(); c++)
+				barr[s][c] = strings[s].charAt(c) == 'X';
+		return barr;
+	}
+	
+	private static enum SixPositions {
+		  ALL(0,0),   SIDES(1,0), EMPTY(2,0),
+		FACES(0,1), CORNERS(1,1),  NONE(2,1);
 		
 		private QuarterTiledImage src;
-
-		private FivePositions(int x, int y) {
+		
+		private SixPositions(int x, int y) {
 			try {
-				this.src = new QuarterTiledImage(ImageIO.read(Tiling.class.getResource("5.png")), TILESIDE, TILESIDE);
+				this.src = new QuarterTiledImage(ImageIO.read(Tiling.class.getResource("grass_6.png")), TILESIDE, TILESIDE);
 			} catch (IOException io) {
 				throw new RuntimeException(io.getMessage());
 			}
